6e3b181c8b4c7cd4643292df8e9eaa0bf581cb5c
luky <lindel33@mail.ru>
2021-12-12 00:33:45 +0300

    new

--- quickstart.py
+++ quickstart.py
@@ -13,4 +13,12 @@ browser.maximize_window()
 browser.get('https://www.instagram.com/')
 time.sleep(5)
 
+button_login = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+button_login.send_keys(user_name)
 
+button = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+button.send_keys(user_pass)
+
+
+
+time.sleep(500)
