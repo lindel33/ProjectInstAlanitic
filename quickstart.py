3aad52b33580355f165b2e62d2fa8d306e5a06a0
luky <lindel33@mail.ru>
2021-12-12 23:48:29 +0300

    new

--- quickstart.py
+++ quickstart.py
@@ -13,11 +13,31 @@ browser.maximize_window()
 browser.get('https://www.instagram.com/')
 time.sleep(5)
 
-button_login = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-button_login.send_keys(user_name)
 
-button = browser.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-button.send_keys(user_pass)
+def set_up(path_html):
+    button_or_form = browser.find_element(By.XPATH, path_html)
+    return button_or_form
+
+
+def log_in():
+    """
+    Функция авторизации и закрытия первых push-сообщений
+    :param:
+    :return:    1 if crash
+                0 if success connect
+    """
+    try:
+        # Логин\пароль
+        set_up('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(user_name)
+        set_up('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(user_pass)
+        # Кнопка войти
+        time.sleep(1)
+        set_up('//*[@id="loginForm"]/div/div[3]').click()
+        # Первое уведомление
+        time.sleep(4)
+        set_up('//*[@id="react-root"]/div/div/section/main/div/div/div/section/div/button').click()
+    except:
+        log_in()
 
 
 
