a495f5161a2f6366da4c7739428074aafb01570e
luky <lindel33@mail.ru>
2021-12-15 20:46:17 +0300

    Дебаг всего Рефакторинг файла quickstart.py

--- authorization/quickstart.py
+++ authorization/quickstart.py
@@ -1,101 +1,154 @@
-from pprint import pprint
-
-from selenium.webdriver import Keys
-from selenium.webdriver.chrome import webdriver
-
-from global_set.settings import user_pass, user_name
+from global_set import settings
 from selenium.webdriver.common.by import By
 from global_set.global_set import get_browser
 import time
 
-
 browser = get_browser()
 
 
+class ObjectMixin:
+    def set_up(self, path_html):
+        """
+        Принимает путь XPath элемента HTML
+        Возвращает объект(ссылку)
+        :param path_html:
+        :return:
+        """
+        button_or_form = browser.find_element(By.XPATH, path_html)
+
+        if button_or_form:
+            return button_or_form
+
+        else:
+            print(path_html)
+            self.set_up(path_html)
 
 
-def set_up(path_html):
-    """
-    Принимает путь XPath элемента HTML
-    Возвращает объект(ссылку)
-    :param path_html:
-    :return:
-    """
-    button_or_form = browser.find_element(By.XPATH, path_html)
+class LoginUser(ObjectMixin):
+    def log_in(self):
+        """
+        Функция авторизации
+        :param:
+        :return:    1 if crash
+                    0 if success connect
+        """
 
-    if button_or_form:
-        return button_or_form
+        count_error = 0
 
-    else:
-        print(path_html)
-        set_up(path_html)
+        try:
+            time.sleep(2)
+            # Логин\пароль
+            self.set_up('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(settings.user_name)
+            self.set_up('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(settings.user_pass)
 
+            # Кнопка войти
+            time.sleep(1)
+            self.set_up('//*[@id="loginForm"]/div/div[3]').click()
+            return 0
+        except None:
+            count_error += 1
 
-def get_posts_user(url_user):
-    time.sleep(3)
-    browser.get(url_user)
-    time.sleep(4)
+            # Чистка полей ввода
+            self.set_up('//*[@id="loginForm"]/div/div[1]/div/label/input').clear()
+            self.set_up('//*[@id="loginForm"]/div/div[2]/div/label/input').clear()
+            self.log_in()
 
-    # links имя div в котором хранятся посты
-    links = browser.find_elements(By.CLASS_NAME, 'v1Nh3.kIKUG._bz0w')
-    all_posts_user = []
-    for el in links:
-        post = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        all_posts_user.append(post)
-
-    time.sleep(2)
-    set_up('//*[@id="react-root"]/div/div/section/main/div/header/section/ul/li[2]/a').click()
-    time.sleep(2)
-
-    FList = browser.find_element_by_css_selector('div[role=\'dialog\'] ul')
-    numberOfFollowersInList = len(FList.find_elements_by_css_selector('li'))
-
-    FList.click()
-    actionChain = webdriver.ActionChains(browser)
-    time.sleep(3)
-
-    while (numberOfFollowersInList < max):
-        actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-        numberOfFollowersInList = len(FList.find_elements_by_css_selector('li'))
-        time.sleep(0.4)
-        print(numberOfFollowersInList)
-        actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-        time.sleep(1)
-
-    return all_posts_user
+            if count_error == 5:
+                print('5 ошибок аторизации')
+                return 1
 
 
+class Scrolling(ObjectMixin):
 
+    def scroll(self):
+        """
+        Функция пролистывания списка подписчиков
+        :return:
+        """
+        end_scroll = True
+        try:
+            time.sleep(2)
+            scr1 = browser.find_element(By.XPATH, '/html/body/div[6]/div/div/div[2]')
+            browser.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
+            return 0
 
-def log_in():
-    """
-    Функция авторизации и закрытия первых push-сообщений
-    :param:
-    :return:    1 if crash
-                0 if success connect
-    """
-    try:
+        except None:
+            if not end_scroll:
+                return 1
+            else:
+                time.sleep(5)
+                self.scroll()
+            return 0.5
+
+    def wait_scroll(self, number_to_scroll):
+        """
+        Нажимаем на кнопку подписчиков и листаем на установленное количесво раз
+        :return:
+        """
         time.sleep(2)
-        # Логин\пароль
-        set_up('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(user_name)
-        set_up('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(user_pass)
+        self.set_up('//*[@id="react-root"]/div/div/section/main/div/header/section/ul/li[2]/a').click()
+        time.sleep(2)
 
-        # Кнопка войти
-        time.sleep(1)
-        set_up('//*[@id="loginForm"]/div/div[3]').click()
+        number_scroll_local = 0
+        count = 0
+        time_wait = 0
+        end = True
+        while end:
+            number_scroll_local += 1
+
+            exit_count = self.scroll()
+            count += int(exit_count)
+            if time_wait == 10:
+                time.sleep(3)
+            if count == 3 or number_scroll_local == number_to_scroll:
+                break
+            time_wait += 1
 
 
+class UserInfo(ObjectMixin):
+    @staticmethod
+    def get_posts_user(url_user):
+        """
+        Функция возвращает список ссылок на посты пользователя
+        :param url_user:
+        """
+        time.sleep(3)
+        browser.get(url_user)
+        time.sleep(4)
+
+        # links имя div в котором хранятся посты
+        links = browser.find_elements(By.CLASS_NAME, 'v1Nh3.kIKUG._bz0w')
+        all_posts_user = []
+        for el in links:
+            post = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            all_posts_user.append(post)
+
+        return all_posts_user
+
+    @staticmethod
+    def get_subscript_links():
+        """
+        Получение списка ссылок на подписчиков пользователя
+        :return: all_sub
+        """
+        # Находим поле с подписчиками
+        subscript = browser.find_elements(By.CSS_SELECTOR, 'body>div.RnEpo.Yx5HN>div>div>div.isgrP>ul>div>li')
+
+        all_subscript = []
+        for sub in subscript:
+            post = sub.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            all_subscript.append(post)
+        return all_subscript
 
 
-    except:
-        print('exceptError')
-
-
-log_in()
-zz = get_posts_user('https://www.instagram.com/astrogks/')
-pprint(zz)
+login = LoginUser()
+login.log_in()
+post_user = UserInfo()
+z = post_user.get_posts_user('https://www.instagram.com/astrogks/')
+print(z)
+scroll = Scrolling()
+scroll.wait_scroll(settings.number_scroll_subscripts)
+x = post_user.get_subscript_links()
+print(x)
 
 time.sleep(500)
-
-
-
