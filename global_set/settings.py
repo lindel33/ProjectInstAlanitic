33971be93ced26ee68e8c88f9ff1c0034406c040
luky <lindel33@mail.ru>
2021-12-15 20:46:17 +0300

    Дебаг всего Рефакторинг файла quickstart.py

--- global_set/settings.py
+++ global_set/settings.py
@@ -1,2 +1,13 @@
 user_name = '89995309522'
 user_pass = 'I1QEvAR1234'
+
+
+wait_open_new_page = 3  # Задержка открытия новой страницы
+
+number_scroll_subscripts = 4  # Сколько раз мы пролистываем список подписок
+
+number_scroll_followers = 4  # Сколько раз мы пролистываем список подписчиков
+
+
+
+
